2cc4d6a4b47d84eed693666ea68e9ab2559deecc
wenyaoxue <119467979+wenyaoxue@users.noreply.github.com>
2025-08-09 20:15:40 -0500

    container height

--- toggleFullscreen.js
+++ toggleFullscreen.js
@@ -8,7 +8,7 @@ function exitFullScreen() {
     document.getElementById("question").style.height = ""
     document.getElementById("question").style.margin = ""
     document.getElementById("container").style.alignContent = ""
-    document.getElementById("container").style.height = ""
+    // document.getElementById("container").style.height = ""
     // document.getElementById("container").style.maxHeight = ""
     document.getElementById("screentoggleimg").src = "fullscreen.png"
     document.getElementById("screentoggle").style.opacity = "0.5"
@@ -22,7 +22,7 @@ function fullScreen() {
     document.getElementById("question").style.height = "70%"
     document.getElementById("question").style.margin = "auto"
     document.getElementById("container").style.alignContent = "center"
-    document.getElementById("container").style.height = "100%"
+    // document.getElementById("container").style.height = "100%"
     // document.getElementById("container").style.maxHeight = "100%"
     document.getElementById("screentoggleimg").src = "exitFullscreen.png"
     function reduceScreenToggleOpacityTo0(opacity) {
