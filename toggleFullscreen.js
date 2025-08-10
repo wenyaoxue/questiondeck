7cd2fdffeb1fc07c13f1ce34d655f26ba9050f03
wenyaoxue <119467979+wenyaoxue@users.noreply.github.com>
2025-08-09 20:18:47 -0500

    other styles on screentoggle

--- toggleFullscreen.js
+++ toggleFullscreen.js
@@ -5,9 +5,9 @@ function exitFullScreen() {
     for (let controlElement of document.getElementsByClassName("controls")) {
         controlElement.style.display = ''
     }
-    document.getElementById("question").style.height = ""
-    document.getElementById("question").style.margin = ""
-    document.getElementById("container").style.alignContent = ""
+    // document.getElementById("question").style.height = ""
+    // document.getElementById("question").style.margin = ""
+    // document.getElementById("container").style.alignContent = ""
     // document.getElementById("container").style.height = ""
     // document.getElementById("container").style.maxHeight = ""
     document.getElementById("screentoggleimg").src = "fullscreen.png"
@@ -19,9 +19,9 @@ function fullScreen() {
     for (let controlElement of document.getElementsByClassName("controls")) {
         controlElement.style.display = 'none'
     }
-    document.getElementById("question").style.height = "70%"
-    document.getElementById("question").style.margin = "auto"
-    document.getElementById("container").style.alignContent = "center"
+    // document.getElementById("question").style.height = "70%"
+    // document.getElementById("question").style.margin = "auto"
+    // document.getElementById("container").style.alignContent = "center"
     // document.getElementById("container").style.height = "100%"
     // document.getElementById("container").style.maxHeight = "100%"
     document.getElementById("screentoggleimg").src = "exitFullscreen.png"
